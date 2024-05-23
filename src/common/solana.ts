094a2eb119068a81fb13f7cb39c44337e1e0f783
avernikoz <avernikoz@gmail.com>
2024-05-23 19:14:08 +0300

    change to mainnet

--- src/common/solana.ts
+++ src/common/solana.ts
@@ -1,1 +1,2 @@
-export const DEFAULT_PROVIDER_URL = "https://fullnode.mainnet.sui.io:443";
+// TODO: Change to mainnet
+export const DEFAULT_PROVIDER_URL = "https://devnet.helius-rpc.com/?api-key=5f8ab112-be5c-4982-aacf-04e3bc1d8dcf";
