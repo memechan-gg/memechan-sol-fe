c367455d0e1f77f0d0cad3c57b5fca2b936b8c7f
zeroxkitakitsune <kitakitsune@kitakitsune.dev>
2024-05-23 08:59:19 -0400

    balance

--- src/hooks/solana/useBalance.ts
+++ src/hooks/solana/useBalance.ts
@@ -1,29 +1,30 @@
-import { MEME_COIN_DECIMALS } from "@/constants/coin";
-import { getCoinsAndNormalizeWithDecimals } from "@/utils";
-import { useCallback, useState } from "react";
+import { Connection, PublicKey } from "@solana/web3.js";
+import { useCallback, useEffect, useState } from "react";
 import { useInterval } from "usehooks-ts";
 
 export const useBalance = (coin: string) => {
   const [balance, setBalance] = useState("0");
-  const account = useCurrentAccount();
-  const provider = useSuiClient();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
-  const fetchBalanceData = useCallback(() => {
-    if (!account || !provider) return;
+  const publicKeyString = "5W22DiXpW1x4s2FKQpDpiai7BfCHzywcyiKWeYJjmwSx"; // Replace with your actual public key
+  const publicKey = new PublicKey(publicKeyString);
+  const connection = new Connection("https://api.devnet.solana.com/");
 
-    getCoinsAndNormalizeWithDecimals({
-      address: account.address,
-      provider,
-      coin,
-      decimals: MEME_COIN_DECIMALS,
-    })
-      .then((data) => {
-        setBalance(data.formatted);
-      })
-      .catch((error) => {
-        console.error("Failed to fetch balance data:", error);
-      });
-  }, [account, provider, coin]);
+  const fetchBalanceData = useCallback(async () => {
+    try {
+      const balance = await connection.getBalance(publicKey);
+      setBalance((balance / 1e9).toString()); // Convert lamports to SOL
+    } catch (err) {
+      setError("Failed to fetch balance");
+    } finally {
+      setLoading(false);
+    }
+  }, [publicKey, connection]);
+
+  useEffect(() => {
+    fetchBalanceData();
+  }, [fetchBalanceData]);
 
   useInterval(fetchBalanceData, 5000);
 
