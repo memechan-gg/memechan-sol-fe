c0aea3312c86374c2741d7e67f6e2b09849734f6
bathord <alutskin@bk.ru>
2024-05-24 23:11:15 +0300

    Create pool types

--- src/types/pool.ts
+++ src/types/pool.ts
@@ -1,8 +1,7 @@
-export type PoolResponse = {
-  objectId: string;
-  typename: string;
-  boundingCurvePackageId: string;
-  quotePackageId: string;
-  quoteCoinType: string;
-  memeCoinType: string;
-};
+import { PoolApiInstance } from "@/common/solana";
+
+export type PoolStatus = "LIVE" | "PRESALE";
+
+export type SeedPoolData = Awaited<ReturnType<typeof PoolApiInstance.getSeedPoolByTokenAddress>>;
+
+export type LivePoolData = Awaited<ReturnType<typeof PoolApiInstance.getLivePoolByTokenAddress>>;
