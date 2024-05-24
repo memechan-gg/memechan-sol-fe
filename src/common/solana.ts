ede6a62fbcaff6e8bd41f218569c80c723616f18
bathord <alutskin@bk.ru>
2024-05-24 23:09:14 +0300

    Create TokenApiInstance & PoolApiInstance

--- src/common/solana.ts
+++ src/common/solana.ts
@@ -1,4 +1,4 @@
-import { Auth, MemechanClient, NoWalletAdapter } from "@avernikoz/memechan-sol-sdk";
+import { Auth, MemechanClient, NoWalletAdapter, PoolAPI, TokenAPI } from "@avernikoz/memechan-sol-sdk";
 
 // TODO: Change to mainnet
 export const DEFAULT_PROVIDER_URL = "https://devnet.helius-rpc.com/?api-key=5f8ab112-be5c-4982-aacf-04e3bc1d8dcf";
@@ -8,6 +8,8 @@ export const IS_TEST_ENV = false;
 export const BE_URL = "https://14r6b4r6kf.execute-api.us-east-1.amazonaws.com/prod";
 
 export const AuthInstance = new Auth(BE_URL);
+export const TokenApiInstance = new TokenAPI(BE_URL);
+export const PoolApiInstance = new PoolAPI(BE_URL);
 export const MemechanClientInstance = new MemechanClient({
   wallet: NoWalletAdapter,
   heliusApiUrl: DEFAULT_PROVIDER_URL,
