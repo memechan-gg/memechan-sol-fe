0622de887af9f9b53988dec45e4a5d9ef4c9415f
bathord <alutskin@bk.ru>
2024-05-24 23:17:39 +0300

    Upd Info comp text & make it use useBoundPoolProgress hook

--- src/views/coin/sidebar/info.tsx
+++ src/views/coin/sidebar/info.tsx
@@ -1,7 +1,9 @@
+import { useBoundPoolProgress } from "@/hooks/solana/useBoundPoolProgress";
 import { InfoProps } from "../coin.types";
 
-export const Info = ({ metadata, progressData }: InfoProps) => {
+export const Info = ({ metadata, poolAddress }: InfoProps) => {
   const { name, symbol, description, image, socialLinks, status } = metadata;
+  const { progress, slerfIn, limit } = useBoundPoolProgress(poolAddress);
 
   return (
     <div className="flex flex-col gap-2">
@@ -9,7 +11,7 @@ export const Info = ({ metadata, progressData }: InfoProps) => {
         <img className="w-32 border border-regular h-auto rounded-lg" src={image} alt="token-image" />
         <div className="flex flex-col gap-1">
           <div className="text-xs font-bold text-regular">
-            {name} (ticker: {symbol})
+            {name} (symbol: {symbol})
           </div>
           <div className="text-xs text-regular">{description}</div>
         </div>
@@ -45,19 +47,30 @@ export const Info = ({ metadata, progressData }: InfoProps) => {
       <div className="flex w-full flex-col gap-1">
         <div className="text-xs flex flex-row gap-2 font-bold text-regular">
           Presale Progress
-          <div className="text-xs font-bold text-regular">{progressData.progress}%</div>
+          <div className="text-xs font-bold text-regular">{progress}%</div>
         </div>
         <div className="w-full bg-white h-4 rounded-lg">
           <div
             className="bg-regular h-full rounded-lg"
             style={{
-              width: `${progressData.progress}%`,
+              width: `${progress}%`,
             }}
           ></div>
         </div>
         <div className="text-regular mt-2">
-          {status === "PRESALE" && "Presale text"}
-          {status === "LIVE" && "Live text"}
+          {status === "PRESALE" && (
+            <div className="flex flex-col gap-2">
+              <div>
+                When the pool reaches {limit} <span className="!normal-case">SLERF</span>, liquidity from the bonding
+                curve will flow exclusively to Raydium Liquidity Pool and be held securely for generating fees.
+              </div>
+              <div>
+                Presently, there is {slerfIn} <span className="!normal-case">SLERF</span>.
+              </div>
+              <div>Happy trading :)</div>
+            </div>
+          )}
+          {status === "LIVE" && "Pool is now live on the Raydium! You can now swap tokens! Happy trading :)"}
         </div>
       </div>
     </div>
