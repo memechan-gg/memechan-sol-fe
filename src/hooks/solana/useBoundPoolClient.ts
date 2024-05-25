64f0f0d33e4bd89a7ee9f0e638398d2368a37713
bathord <alutskin@bk.ru>
2024-05-25 23:08:58 +0300

    Create & update hooks

---
+++ src/hooks/solana/useBoundPoolClient.ts
@@ -0,0 +1,25 @@
+import { MemechanClientInstance } from "@/common/solana";
+import { BoundPoolClient } from "@avernikoz/memechan-sol-sdk";
+import { PublicKey } from "@solana/web3.js";
+import useSWR from "swr";
+
+const fetchBoundPoolClient = async (poolAddress: string) => {
+  try {
+    const boundPool = await BoundPoolClient.fromBoundPoolId({
+      client: MemechanClientInstance,
+      poolAccountAddressId: new PublicKey(poolAddress),
+    });
+
+    return boundPool;
+  } catch (e) {
+    console.error(`[fetchBoundPoolClient] Failed to get bound pool client ${poolAddress}:`, e);
+  }
+};
+
+export function useBoundPoolClient(poolAddress: string) {
+  const { data } = useSWR([`bound-pool-client-${poolAddress}`, poolAddress], ([url, pool]) =>
+    fetchBoundPoolClient(pool),
+  );
+
+  return data;
+}
