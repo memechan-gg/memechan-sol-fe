8dc0ab253217af828f7b051501e615088f8a7837
bathord <alutskin@bk.ru>
2024-05-26 19:22:26 +0300

    Create Presale Holders & Live Holders comps

--- src/views/coin/sidebar/holders/presale-coin-holders.tsx
+++ src/views/coin/sidebar/holders/presale-coin-holders.tsx
@@ -1,21 +1,10 @@
-import { useUniqueHolders } from "@/hooks/solana/useUniqueHolders";
+import { usePresaleCoinUniqueHolders } from "@/hooks/presale/usePresaleCoinUniqueHolders";
 import { FULL_MEME_AMOUNT_CONVERTED, MEMECHAN_MEME_TOKEN_DECIMALS } from "@avernikoz/memechan-sol-sdk";
 import BigNumber from "bignumber.js";
-import { HoldersProps } from "../coin.types";
+import { HoldersProps } from "../../coin.types";
 
-export const Holders = ({ poolAddress, coinMetadata }: HoldersProps) => {
-  const uniqueHolders = useUniqueHolders(poolAddress);
-
-  const renderHolderType = (type: "bonding_curve" | "dev" | string | undefined) => {
-    switch (type) {
-      case "bonding_curve":
-        return "(bonding curve)";
-      case "dev":
-        return "(dev)";
-      default:
-        return "";
-    }
-  };
+export const PresaleCoinHolders = ({ poolAddress, coinMetadata }: HoldersProps) => {
+  const uniqueHolders = usePresaleCoinUniqueHolders(poolAddress);
 
   return (
     <div className="flex flex-col gap-1">
@@ -30,17 +19,19 @@ export const Holders = ({ poolAddress, coinMetadata }: HoldersProps) => {
 
             const percentage = ticketsMemeAmount.div(FULL_MEME_AMOUNT_CONVERTED).multipliedBy(100).toFixed(2);
             const address = holder.slice(0, 6) + "..." + holder.slice(-4);
-            const type = coinMetadata.creator === holder ? "dev" : undefined;
+            const holderIsDev = coinMetadata.creator === holder;
 
             return (
               <div key={address} className="flex justify-between flex-row gap-2 text-xs font-bold text-regular">
                 <div>
-                  <span className="font-normal">{address}</span> {renderHolderType(type)}
+                  <span className="font-normal">{address}</span> {holderIsDev ? "(dev)" : ""}
                 </div>
                 <div>{percentage}%</div>
               </div>
             );
           })}
+        {uniqueHolders && uniqueHolders.size === 0 && <div className="font-normal">No holders yet.</div>}
+        {!uniqueHolders && <div className="font-normal">Loading...</div>}
       </div>
     </div>
   );
