655ee72ed6a29e5df6e5b059a12bf79e039dca7e
bathord <alutskin@bk.ru>
2024-05-27 14:47:58 +0300

    Implement handleErrors(), validateCoinParams() methods

--- src/views/create-coin/create-coin.utils.ts
+++ src/views/create-coin/create-coin.utils.ts
@@ -1,32 +1,36 @@
 import { AuthInstance, MemechanClientInstance, TokenApiInstance } from "@/common/solana";
-import { ADMIN_PUB_KEY, BoundPoolClient, MEMECHAN_QUOTE_TOKEN, TokenAPI } from "@avernikoz/memechan-sol-sdk";
+import {
+  ADMIN_PUB_KEY,
+  BoundPoolClient,
+  InvalidCoinDescriptionError,
+  InvalidCoinImageError,
+  InvalidCoinNameError,
+  InvalidCoinSymbolError,
+  MEMECHAN_QUOTE_TOKEN,
+  NameEqualsToDescriptionError,
+  SymbolEqualsToDescriptionError,
+  validateCreateCoinParams,
+} from "@avernikoz/memechan-sol-sdk";
 import { PublicKey } from "@solana/web3.js";
 import toast from "react-hot-toast";
 import { ICreateForm } from "./create-coin.types";
-// TODO: Handle coin 
-// https://explorer.solana.com/tx/3GpoZFSDbQiNtjvXRN42wxAMZxFrEUDkTa4qniy2c33xsGi5S4qQAsGbmgyrqH3G1S8yuGVGRjemaYUhR8pYyqU?cluster=devnet
+
 export function handleErrors(e: unknown) {
-  /*if (e instanceof InvalidCoinNameError) {
+  if (e instanceof InvalidCoinNameError) {
     return toast.error("Invalid coin name");
   } else if (e instanceof InvalidCoinSymbolError) {
     return toast.error("Invalid coin symbol");
-  } else if (e instanceof InvalidCoinDecimalsError) {
-    return toast.error("Invalid coin decimals");
-  } else if (e instanceof InvalidCoinTotalSupplyError) {
-    return toast.error("Invalid coin total supply");
   } else if (e instanceof InvalidCoinDescriptionError) {
     return toast.error("Invalid coin description");
   } else if (e instanceof InvalidCoinImageError) {
     return toast.error("Invalid coin image");
-  } else if (e instanceof InvalidSignerAddressError) {
-    return toast.error("Invalid signer address");
   } else if (e instanceof SymbolEqualsToDescriptionError) {
-    return toast.error("Symbol equals to description, please provide a different symbol");
+    return toast.error("Symbol equals to description, please provide a different symbol or description");
   } else if (e instanceof NameEqualsToDescriptionError) {
-    return toast.error("Name equals to description, please provide a different name");
-  }*/
+    return toast.error("Name equals to description, please provide a different name or description");
+  }
 
-  return toast.error("An error occurred while creating meme coin, please try again");
+  return toast.error("Unrecognized error occurred while creating meme coin. Please try again");
 }
 
 export async function createCoinOnBE({ discord, telegram, twitter, website }: ICreateForm, signature: string) {
@@ -53,8 +57,6 @@ export async function createMemeCoin(data: ICreateForm, publicKey: PublicKey, ip
   });
 }
 
-export async function createBondingCurvePool() {}
-
 export async function handleAuthentication(address: string, sign: (message: Uint8Array) => Promise<Uint8Array>) {
   const messageToSign = await AuthInstance.requestMessageToSign(address);
   const encodedMessage = new TextEncoder().encode(messageToSign);
@@ -73,4 +75,15 @@ export async function uploadImageToIPFS(file: File) {
   return `https://lavender-gentle-primate-223.mypinata.cloud/ipfs/${result.IpfsHash}?pinataGatewayToken=M45Jh03NicrVqTZJJhQIwDtl7G6fGS90bjJiIQrmyaQXC_xXj4BgRqjjBNyGV7q2`;
 }
 
-export function validateCoinParams(data: ICreateForm, address: string, ipfsUrl: string) {}
+export function validateCoinParamsWithoutImage(data: ICreateForm) {
+  validateCreateCoinParams({
+    name: data.name,
+    description: data.description,
+    image: "https://mock.com",
+    symbol: data.symbol,
+  });
+}
+
+export function validateCoinParamsWithImage(data: ICreateForm, ipfsUrl: string) {
+  validateCreateCoinParams({ name: data.name, description: data.description, image: ipfsUrl, symbol: data.symbol });
+}
