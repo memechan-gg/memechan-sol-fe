f1fca05108a98537302f926eb515bb81aadc0eac
zeroxkitakitsune <kitakitsune@kitakitsune.dev>
2024-05-27 16:16:22 -0400

    added skeleton and increased the size of chart

--- src/components/chart-iframe.tsx
+++ src/components/chart-iframe.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 interface ChartIframeProps {
   address: string;
@@ -24,21 +26,27 @@ export const ChartIframe = ({ address, symbol, chartsApiUrl }: ChartIframeProps)
   };
 
   return (
-    <div className={`h-96 w-full flex items-center justify-center ${!isLoaded ? "bg-regular" : ""}`}>
+    <div style={{ height: "38rem" }} className={`w-full flex items-center justify-center`}>
       {!hasError ? (
-        <iframe
-          allowFullScreen
-          style={{ borderWidth: 0 }}
-          src={`https://${chartsApiUrl}/?address=${address}&symbol=${symbol}`}
-          width="100%"
-          height="100%"
-          onLoad={handleLoad}
-          onError={handleError}
-        />
+        <>
+          <iframe
+            allowFullScreen
+            style={{ borderWidth: 0, display: isLoaded ? "block" : "none" }}
+            src={`https://${chartsApiUrl}/?address=${address}&symbol=${symbol}`}
+            width="100%"
+            height="100%"
+            onLoad={handleLoad}
+            onError={handleError}
+          />
+          {!isLoaded && (
+            <div className="w-full h-full">
+              <Skeleton width="100%" height="100%" className="skeleton-custom" />
+            </div>
+          )}
+        </>
       ) : (
         <div>Error loading chart</div>
       )}
-      {!isLoaded && !hasError && <div>Loading...</div>}
     </div>
   );
 };
