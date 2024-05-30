77857caa7532ef11532665743d37b9590f6adf9b
avernikoz <avernikoz@gmail.com>
2024-05-31 01:22:18 +0300

    change name of NEXT_PUBLIC_CHARTS_API_HOSTNAME again

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import {withSentryConfig} from '@sentry/nextjs';
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_CHARTS_API_HOSTNAME: "memechan-gg-chart.netlify.app",
+    NEXT_PUBLIC_CHARTS_API_HOSTNAME: "memechan-chart.netlify.app",
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
