333be9f70b8667fe10a1fad36682d92ed56a0a75
avernikoz <avernikoz@gmail.com>
2024-06-03 18:45:16 +0300

    upd rpc endpoints

--- src/common/endpoints.ts
+++ src/common/endpoints.ts
@@ -1,3 +1,5 @@
-export const endpoints = ["https://mainnet.helius-rpc.com/?api-key=923b03cb-c57e-4942-a22d-05331e591fdf"];
+// export const endpoints = ["https://mainnet.helius-rpc.com/?api-key=923b03cb-c57e-4942-a22d-05331e591fdf"];
 
 // export const endpoints = ["https://helius-enhanced-proxy.avernikoz.workers.dev"];
+
+export const endpoints = ["https://rpc1.memechan.xyz"];
