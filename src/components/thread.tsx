14216b6fcc4a135eed7aeadd8cf9c83146a50759
bathord <bathord43@gmail.com>
2024-06-08 00:51:45 +0300

    Upd Thread comp params passing

--- src/components/thread.tsx
+++ src/components/thread.tsx
@@ -1,9 +1,10 @@
+import { CoinMetadata } from "@/types/coin";
 import { formatNumber } from "@/utils/formatNumber";
 import Link from "next/link";
 
 export function NoticeBoard({ title, children }: { title: string; children: React.ReactNode }) {
   return (
-    <div className="flex flex-col border border-solid border-black w-full">
+    <div className="flex flex-col border border-solid border-black w-full sm:text-start text-center">
       {/* Title part of the board */}
       <div className="bg-regular text-white pl-2 py-1 font-bold">
         <h2 className="text-sm">{title}</h2>
@@ -28,8 +29,8 @@ export function ThreadBoard({
   return (
     <div className="flex flex-col border border-solid border-black w-full">
       {/* Title part of the board */}
-      <div className="bg-board text-regular flex items-center flex-row pl-2 py-1 font-bold justify-between">
-        <h2 className="text-sm">{title}</h2>
+      <div className="bg-board text-regular flex items-center p-2 sm:pb-2 pb-3 font-bold justify-between sm:flex-row flex-col">
+        <h2 className="sm:text-sm text-xl sm:mb-0 mb-4">{title}</h2>
         {titleChildren}
       </div>
       {/* Body part of the board */}
@@ -41,28 +42,16 @@ export function ThreadBoard({
 }
 
 export function Thread({
-  title,
-  mint,
-  image,
-  createdBy,
-  marketCap,
-  ticker,
-  description,
+  coinMetadata: { name, address, image, creator, marketcap, symbol, description },
 }: {
-  title: string;
-  mint: string;
-  image: string;
-  createdBy: string;
-  marketCap: number;
-  ticker: string;
-  description: string;
+  coinMetadata: CoinMetadata;
 }) {
   return (
     <div className="flex flex-col gap-2">
       <div className="w-[150px]">
-        <h2 className="text-sm font-bold text-regular truncate">{title}</h2>
+        <h2 className="text-sm font-bold text-regular truncate">{name}</h2>
       </div>
-      <Link href={`/coin/${mint}`}>
+      <Link href={`/coin/${address}`}>
         <img
           className="w-[150px] border border-regular h-[150px] object-cover object-center"
           src={image}
@@ -72,14 +61,14 @@ export function Thread({
       <div className="flex flex-col gap-1 text-xs">
         <div className="text-link">
           Created by:{" "}
-          <Link href={`/profile/${createdBy}`}>
-            <span className="font-bold hover:underline">{createdBy.slice(0, 5) + "..." + createdBy.slice(-3)}</span>
+          <Link href={`/profile/${creator}`}>
+            <span className="font-bold hover:underline">{creator.slice(0, 5) + "..." + creator.slice(-3)}</span>
           </Link>
         </div>
-        <div className="text-green">market cap: {formatNumber(marketCap, 2)}</div>
-        <Link href={`/coin/${mint}`}>
+        <div className="text-green">market cap: {formatNumber(marketcap, 2)}</div>
+        <Link href={`/coin/${address}`}>
           <div className="text-regular flex flex-col flex-wrap">
-            <div className="font-bold !normal-case">symbol: {ticker}</div>
+            <div className="font-bold !normal-case">symbol: {symbol}</div>
             <div className="max-w-[150px] truncate">{description}</div>
           </div>
         </Link>
