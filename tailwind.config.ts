8b53ebe48bed754559c51c7c0b78eb72c07a60ad
bathord <bathord43@gmail.com>
2024-06-10 16:19:40 +0300

    Add new tailwind screen sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/views/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/views/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -43,6 +44,11 @@ const config: Config = {
         quote: "#030080",
         blue: "#0047FF",
       },
+    },
+    screens: {
+      xxs: "350px",
+      xs: "475px",
+      ...defaultTheme.screens,
     },
   },
   plugins: [],
