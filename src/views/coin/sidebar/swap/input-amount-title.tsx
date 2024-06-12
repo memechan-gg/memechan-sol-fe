bc3bff40dcdb2b72b46e41f7d5cf5dcfb435d470
bathord <bathord43@gmail.com>
2024-06-12 13:25:09 +0300

    Create InputAmountButtons comp

--- src/views/coin/sidebar/swap/input-amount-title.tsx
+++ src/views/coin/sidebar/swap/input-amount-title.tsx
@@ -3,8 +3,8 @@ import {
   MEMECHAN_QUOTE_TOKEN_DECIMALS,
   SwapMemeOutput,
 } from "@avernikoz/memechan-sol-sdk";
-import BigNumber from "bignumber.js";
 import { Dispatch, SetStateAction } from "react";
+import { InputAmountButtons } from "./input-amount-buttons";
 
 export const InputAmountTitle = ({
   slerfToMeme,
@@ -21,59 +21,20 @@ export const InputAmountTitle = ({
   setInputAmount: Dispatch<SetStateAction<string>>;
   setOutputData: Dispatch<SetStateAction<SwapMemeOutput | null>> | Dispatch<SetStateAction<string | null>>;
 }) => {
-  const handleInputAmountPercentButtonClick = (percent: number | string) => {
-    const inputValue = slerfToMeme ? slerfBalance : memeBalance;
-    const inputDecimals = slerfToMeme ? MEMECHAN_QUOTE_TOKEN_DECIMALS : MEMECHAN_MEME_TOKEN_DECIMALS;
-
-    if (!inputValue) {
-      setInputAmount("");
-      return;
-    }
-
-    const partialAmount = new BigNumber(inputValue).multipliedBy(percent).div(100).toFixed(inputDecimals);
-
-    setInputAmount(partialAmount);
-  };
-
-  const handleResetClick = () => {
-    setInputAmount("");
-    setOutputData(null);
-  };
+  const inputValue = slerfToMeme ? slerfBalance : memeBalance;
+  const inputDecimals = slerfToMeme ? MEMECHAN_QUOTE_TOKEN_DECIMALS : MEMECHAN_MEME_TOKEN_DECIMALS;
 
   return (
     <div className="flex flex-wrap justify-between">
       <div className="text-xs font-bold text-regular">
         {slerfToMeme ? `SLERF to ${tokenSymbol}` : `${tokenSymbol} to SLERF`}
       </div>
-      <div className="flex items-self-end gap-3 justify-self-end mr-1">
-        <div className="text-regular hover:underline cursor-pointer font-bold" onClick={handleResetClick}>
-          reset
-        </div>
-        <div
-          className="text-regular hover:underline cursor-pointer font-bold"
-          onClick={() => handleInputAmountPercentButtonClick(25)}
-        >
-          25%
-        </div>
-        <div
-          className="text-regular hover:underline cursor-pointer font-bold"
-          onClick={() => handleInputAmountPercentButtonClick(50)}
-        >
-          50%
-        </div>
-        <div
-          className="text-regular hover:underline cursor-pointer font-bold"
-          onClick={() => handleInputAmountPercentButtonClick(75)}
-        >
-          75%
-        </div>
-        <div
-          className="text-regular hover:underline cursor-pointer font-bold"
-          onClick={() => handleInputAmountPercentButtonClick(100)}
-        >
-          max
-        </div>
-      </div>
+      <InputAmountButtons
+        setInputAmount={setInputAmount}
+        setOutputAmount={setOutputData}
+        maxAmount={inputValue}
+        decimals={inputDecimals}
+      />
     </div>
   );
 };
