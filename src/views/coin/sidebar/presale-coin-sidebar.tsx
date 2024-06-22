e831417c0072f75c099e86c4468340d5ec40b077
harunskender97 <harun.skender@outlook.com>
2024-06-22 16:02:20 +0200

    Fix pre-sale pages

--- src/views/coin/sidebar/presale-coin-sidebar.tsx
+++ src/views/coin/sidebar/presale-coin-sidebar.tsx
@@ -1,5 +1,4 @@
 import { useBoundPool } from "@/hooks/presale/useBoundPool";
-import { useBoundPoolClient } from "@/hooks/presale/useBoundPoolClient";
 import { PresaleCoinSidebarProps } from "../coin.types";
 import { PresaleCoinHolders } from "./holders/presale-coin-holders";
 import { PresaleCoinInfo } from "./info/presale-coin-info";
@@ -8,11 +7,7 @@ import { PresaleCoinSwap } from "./swap/presale-coin-swap";
 
 export function PresaleCoinSidebar({ pool, coinMetadata, uniqueHoldersData, ticketsData }: PresaleCoinSidebarProps) {
   const boundPool = useBoundPool(pool.address);
-  const boundPoolClient = useBoundPoolClient(pool.address);
 
-  if (!boundPool) {
-    return <>Loading...</>;
-  }
   return (
     <>
       <SidebarItem>
