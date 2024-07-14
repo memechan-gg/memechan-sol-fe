04bbd612b516eac96ee3113012f3601da76b5586
Harun <100675989+harunskender97@users.noreply.github.com>
2024-07-14 19:59:51 +0200

    local storage hotfix

--- src/context/utils.ts
+++ src/context/utils.ts
@@ -1,5 +1,6 @@
 import { MEMECHAN_RPC_ENDPOINT } from "@/config/config";
 
 export const getInitialRpcEndpoint = () => {
-  return (typeof window !== "undefined" && localStorage.getItem("rpc-endpoint")) || MEMECHAN_RPC_ENDPOINT;
+  // update this later to fetch from local storage
+  return (typeof window !== "undefined" && MEMECHAN_RPC_ENDPOINT) || MEMECHAN_RPC_ENDPOINT;
 };
