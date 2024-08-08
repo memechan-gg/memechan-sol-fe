c0698d79051781fe2b0c62e4dd8a069295f58ed9
Harun <100675989+harunskender97@users.noreply.github.com>
2024-08-08 13:33:04 +0200

    add tokencard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,37 @@
 import { searchAtom } from "@/atoms";
 import { BE_URL } from "@/common/solana";
+import { TokenCard } from "@/components/TokenCard";
+import { Typography } from "@/memechan-ui/Atoms/Typography";
 import { Home } from "@/views/home";
+import { SolanaToken } from "@avernikoz/memechan-sol-sdk";
 import { useQuery } from "@tanstack/react-query";
 import { useRecoilValue } from "recoil";
 
 export default function HomePage() {
   const search = useRecoilValue(searchAtom);
 
-  return search ? <SearchModal search={search} /> : <Home />;
+  return search ? <SearchModal /> : <Home />;
 }
 
-const SearchModal = ({ search }: { search: string }) => {
-  const { data } = useQuery({ queryKey: ["token", "search", search], queryFn: () => getSearch(search) });
-  console.log(data);
-  return <>{JSON.stringify(data)}</>;
+const SearchModal = () => {
+  const search = useRecoilValue(searchAtom);
+  const { data: tokens, isLoading } = useQuery<SolanaToken[]>({
+    queryKey: ["token", "search", search],
+    queryFn: () => getSearch(search),
+  });
+  const isCoinsListEmpty = tokens?.length === 0 && !isLoading;
+
+  // todo:debounce:
+  // todo:put modal somewhere so its availiable on all pages
+  return (
+    <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3 justify-center w-full">
+      {isLoading && <Typography variant="h4">Loading...</Typography>}
+      {tokens?.map((token) => (
+        <TokenCard key={`${token.address}`} token={token} showCheckmark disableContent showOnClick />
+      ))}
+      {isCoinsListEmpty && <Typography>No memecoins yet</Typography>}
+    </div>
+  );
 };
 
 const getSearch = async (search: string) => {
