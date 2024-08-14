13fec670942caced9743aa7971951502796f3239
zeroxkitakitsune <kitakitsune@kitakitsune.dev>
2024-08-14 14:31:56 +0300

    integrated moongate wallet adapter

--- src/components/provider/solana.tsx
+++ src/components/provider/solana.tsx
@@ -1,7 +1,27 @@
 import { MEMECHAN_RPC_ENDPOINT } from "@/config/config";
+import { registerMoonGateWallet } from "@moongate/moongate-adapter";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { PhantomWalletAdapter, SolflareWalletAdapter, TrustWalletAdapter } from "@solana/wallet-adapter-wallets";
+
+registerMoonGateWallet({
+  authMode: "Google",
+  position: "top-right",
+  logoDataUri: "OPTIONAL ADD IN-WALLET LOGO URL HERE",
+  buttonLogoUri: "ADD OPTIONAL LOGO FOR WIDGET BUTTON HERE",
+});
+registerMoonGateWallet({
+  authMode: "Twitter",
+  position: "top-right",
+  logoDataUri: "OPTIONAL ADD IN-WALLET LOGO URL HERE",
+  buttonLogoUri: "ADD OPTIONAL LOGO FOR WIDGET BUTTON HERE",
+});
+registerMoonGateWallet({
+  authMode: "Apple",
+  position: "top-right",
+  logoDataUri: "OPTIONAL ADD IN-WALLET LOGO URL HERE",
+  buttonLogoUri: "ADD OPTIONAL LOGO FOR WIDGET BUTTON HERE",
+});
 
 const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter(), new TrustWalletAdapter()];
 
